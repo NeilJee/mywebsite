9949a5a8fa38bc290727c6c26af9cc190241598c
neil <66482254@qq.com>
2015-05-20 12:14:42 +0800

    all

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/users', function(req, res, next) {
+router.post('/', function(req, res, next) {
     res.send('DSFsfas');
 });
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
